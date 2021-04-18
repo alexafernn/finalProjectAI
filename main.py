3d43d988ab3845d6cd803e0a5aae15dab5ef3713
Alexa Fernandez <alexafernn@Alexas-MacBook-Air.local>
2021-04-18 17:38:09 -0400

    updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+import sys
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
@@ -9,6 +10,7 @@ from sklearn.naive_bayes import MultinomialNB
 #load into a numpy array and print shape
 import pandas as pd
 df = pd.read_csv("billionaires.csv", sep=",")
+
 
 #Part4:  your input or output features are categorical and represented as strings,
 # some models my require you to re-encode them as integers. In this case, add the line
@@ -19,6 +21,7 @@ df = pd.read_csv("billionaires.csv", sep=",")
 #columns we need to change from string
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 le = LabelEncoder()
+df['demographics.age'] = df['demographics.age'].abs()
 df['name'] = le.fit_transform(df['name'])
 df['company.name'] = le.fit_transform(df['company.name'].astype(str))
 df['company.relationship'] = le.fit_transform(df['company.relationship'].astype(str))
@@ -36,12 +39,22 @@ df['wealth.how.inherited'] = le.fit_transform(df['wealth.how.inherited'].astype(
 df['wealth.how.was founder'] = le.fit_transform(df['wealth.how.was founder'].astype(str))
 df['wealth.how.was political'] = le.fit_transform(df['wealth.how.was political'].astype(str))
 
-print(df.head(5))
+# rounding to nearest billion for wealth.worth in billions
+df['wealth.worth in billions'] = df['wealth.worth in billions'].apply(lambda x: round(x, 0))
+df['wealth.worth in billions'] = df['wealth.worth in billions'].astype(int)
+
+df['location.gdp'] = df['location.gdp'].apply(lambda x: round(x, 0))
+df['v'] = df['location.gdp'].astype(int)
+
+print(df.head(15))
+# print(df.dtypes)
 
 
 numpy_array = df.to_numpy()
 # print(numpy_array)
 print(numpy_array.shape)
+np.set_printoptions(threshold=sys.maxsize)
+print(numpy_array>=0)
 
 #if you are applying supervised learning, split your data into input features X and output Y
 print("about to print y aka outputs")
@@ -57,6 +70,11 @@ print(x.shape)
 #Part 3: Split your data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
 
+print("printing shapes")
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
 
 #part 5: apply basic technique
 #how many categories the other features have and use the average of that. -- for bins for #wealth.worth in billions
@@ -83,7 +101,7 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random
 # col20Count = df['location.gdp'].nunique()
 #
 # print(col1Count)
-print(col2Count)
+# print(col2Count)
 
 
 
@@ -91,3 +109,11 @@ print(col2Count)
 # nb_classifier = MultinomialNB()
 # y_prediction = nb_classifier.fit(X_train, y_train).predict(X_test)
 # print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_prediction).sum()))
+
+#part 5:
+nb_classifier = MultinomialNB(alpha=1.0, fit_prior=True, class_prior=None)
+nb_classifier.fit(X_train, y_train)
+print("mean accuracy on given train data and labels", nb_classifier.score(X_train,y_train))
+print("mean accuracy on give test data and labels", nb_classifier.score(X_test,y_test))
+
+#
